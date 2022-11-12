4e74d2165992ec882368c82a13ab0f17d62ce455
abogo-nono <abogonono1@gmail.com>
2022-11-12 01:48:52 +0100

    -a

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 
-    // Only process POST reqeusts.
+    // Only process POST requests.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
